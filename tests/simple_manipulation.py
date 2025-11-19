a665dd0ed05cdf45e04d25740cddfc65dfefb498
Alexander Christensen <alexchristensen553@gmail.com>
2025-11-19 15:54:55 +0100

    Changed "load_image" to work from anywhere

--- tests/simple_manipulation.py
+++ tests/simple_manipulation.py
@@ -1,14 +1,21 @@
-import os
+from pathlib import Path
 import numpy as np
 import cv2 as cv
 
-BILLED_MAPPE = os.path.join(os.path.dirname(__file__), "Sample_Images")
+# Variables to locate "Sample_images" folder
+SCRIPT_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = SCRIPT_DIR.parent
+PICTURE_FOLDER = PROJECT_ROOT / "data" / "Sample_images"
+
 
 def load_image(filename):
-    """Always loads images from the 'billeder' folder."""
-    path = os.path.join(BILLED_MAPPE, filename)
+    """Always loads images from the 'Sample_images' folder."""
+    path = PICTURE_FOLDER / filename
     img = cv.imread(path)
     assert img is not None, f"Could not read image: {path}"
     return img
 
-img = load_image("Square_test.jpg")
+img = load_image("Sample2.jpg")
+
+cv.imshow("Sample2", img)
+cv.waitKey(0)
