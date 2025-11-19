82ef7a78d95b1f8f3cbc9328ce8239fee0965dd2
Alexander Christensen <alexchristensen553@gmail.com>
2025-11-19 17:01:42 +0100

    Added function to resize images and display image size

--- tests/simple_manipulation.py
+++ tests/simple_manipulation.py
@@ -7,7 +7,6 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = SCRIPT_DIR.parent
 PICTURE_FOLDER = PROJECT_ROOT / "data" / "Sample_images"
 
-
 def load_image(filename):
     """Always loads images from the 'Sample_images' folder."""
     path = PICTURE_FOLDER / filename
@@ -15,7 +14,25 @@ def load_image(filename):
     assert img is not None, f"Could not read image: {path}"
     return img
 
-img = load_image("Sample2.jpg")
+def rescalePicture(frame, scale=0.4):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    
+    dimensions = (width,height)
+    
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-cv.imshow("Sample2", img)
+img = load_image("Sample2.jpg")
+resized_image = (rescalePicture(img))
+h, w = img.shape[:2]
+Picture_size_text = f"{h} x {w}"
+font = cv.FONT_ITALIC
+
+cv.putText(resized_image,
+           f"Image Size: {Picture_size_text}",
+           (10,30), 
+           font, 1,(255,255,255),2,cv.LINE_AA)
+cv.imshow("Sample2", resized_image)
+
+print(img.shape)
 cv.waitKey(0)
