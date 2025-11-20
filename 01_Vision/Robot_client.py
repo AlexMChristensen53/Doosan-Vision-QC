71219e53dd9b9ba98705d75b7ff592fc8c2c2e26
Alexander Christensen <alexchristensen553@gmail.com>
2025-11-20 20:24:30 +0100

    updating

---
+++ 01_Vision/Robot_client.py
@@ -0,0 +1,6 @@
+"""Robot client helpers for sending TP (teach pendant) commands to Doosan.
+
+Denne fil indeholder en lille hjælperfunktion `send_tp_pop` som bygger
+en TP-kommando til at vise en popup på robotten via `tp_pop(...)`.
+"""
+
