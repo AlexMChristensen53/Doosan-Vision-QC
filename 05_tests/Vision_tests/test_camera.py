c6b1b6e940238594972abf9d24f02f1c3e40fa61
Alexander Christensen <alexchristensen553@gmail.com>
2025-11-25 13:59:42 +0100

    WIP Camera Manual Focus

--- 05_tests/Vision_tests/test_camera.py
+++ 05_tests/Vision_tests/test_camera.py
@@ -1,29 +1,67 @@
 import cv2 as cv
 import depthai as dai
 import numpy as np
-from transformations import rotation
+#from transformations import rotation
+import time
+
+focus_value = 150
 
 pipeline = dai.Pipeline()
 
 cam = pipeline.createColorCamera()
-cam.setPreviewSize(640, 400)
+cam.setIspScale(1, 1)
 cam.setInterleaved(False)
+
+control = pipeline.createXLinkIn()
+control.setStreamName("Control")
+control.out.link(cam.inputControl)
 
 xout = pipeline.createXLinkOut()
 xout.setStreamName("video")
-cam.preview.link(xout.input)
+cam.isp.link(xout.input)
 
 with dai.Device(pipeline) as device:
     q = device.getOutputQueue("video", 4, False)
+    
+    ControlQueue = device.getInputQueue(name="Control")
+    
+    ctrl = dai.CameraControl()
+    ctrl.setManualFocus(50)
+    ControlQueue.send(ctrl)
+
+    print(f"Initial Focus Value: ", focus_value)
 
     while True:
         frame = q.get().getCvFrame()
-        rotated = rotation(frame, 180)
-        blur = cv.GaussianBlur(rotated, (7, 7), 5)
-        edges = cv.Canny(blur, 100, 200)
+        cv.imshow("Livefeed, rotated", frame)
+        
+        focus_value = max(0, min(focus_value, 255))
 
-        cv.imshow("Livefeed, rotated", edges)
-        if cv.waitKey(1) == ord('q'):
+        
+        key = cv.waitKey(1)
+        
+        if key == ord("."):
+            focus_value += 5
+            ctrl = dai.CameraControl()
+            ctrl.setManualFocus(50)
+            ControlQueue.send(ctrl)
+            print(f"New Focus Value: ", focus_value)
+            
+        elif key == ord(","):
+            focus_value -= 5
+            ctrl = dai.CameraControl()
+            ctrl.setManualFocus(50)
+            ControlQueue.send(ctrl)
+            print(f"New Focus Value: ", focus_value)
+        
+            
+        elif key == ord('q'):
             break
+        
+        #rotated = rotation(frame, 180)
+        #blur = cv.GaussianBlur(rotated, (7, 7), 5)
+        #edges = cv.Canny(blur, 100, 200)
+
+        
 
 
